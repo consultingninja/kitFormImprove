1aee18deee833e8745de08fefdaee57ecf9613c5
consultingninja <consultingninja.tech@gmail.com>
2023-06-23 21:05:45 -0500

    update main page message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Welcome to Consulting Ninja!</h1>
-<h3>Improving SvelteKit Forms UX!</h3>
+<h3>Improving the UX of SvelteKit Forms!</h3>
 <p>Visit <a href="https://www.youtube.com/@ConsultingNinja/featured"><strong>@ConsultingNinja</strong></a> to see more great videos!</p>
 
 
